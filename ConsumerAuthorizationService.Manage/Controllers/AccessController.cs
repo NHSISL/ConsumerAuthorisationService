372d73e267939ba08010352bedefa6b2744f9a9e
Christo du Toit <christo@dutoit.co.uk>
2026-06-15 10:48:22 +0100

    ShouldReturnOkOnPostAsync -> PASS

--- ConsumerAuthorizationService.Manage/Controllers/AccessController.cs
+++ ConsumerAuthorizationService.Manage/Controllers/AccessController.cs
@@ -5,6 +5,7 @@
 using System;
 using System.Threading.Tasks;
 using ConsumerAuthorizationService.Core.Models.Foundations.Accesses;
+using ConsumerAuthorizationService.Core.Models.Orchestrations.Accesses.Exceptions;
 using ConsumerAuthorizationService.Core.Services.Orchestrations.Accesses;
 using ConsumerAuthorizationService.Manage.Models.Accesses;
 using Microsoft.AspNetCore.Authorization;
@@ -27,7 +28,31 @@ namespace ConsumerAuthorizationService.Manage.Controllers
         public async ValueTask<ActionResult<Access>> PostAccessValidationAsync(
             [FromBody] ValidateAccessRequest request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                Access access = await this.accessOrchestrationService.ValidateAccess(
+                    request.ConsumerUserId,
+                    request.NhsNumber,
+                    request.CorrelationId);
+
+                return Ok(access);
+            }
+            catch (AccessOrchestrationValidationException accessOrchestrationValidationException)
+            {
+                return BadRequest(accessOrchestrationValidationException.InnerException);
+            }
+            catch (AccessOrchestrationDependencyValidationException accessOrchestrationDependencyValidationException)
+            {
+                return BadRequest(accessOrchestrationDependencyValidationException.InnerException);
+            }
+            catch (AccessOrchestrationDependencyException accessOrchestrationDependencyException)
+            {
+                return InternalServerError(accessOrchestrationDependencyException);
+            }
+            catch (AccessOrchestrationServiceException accessOrchestrationServiceException)
+            {
+                return InternalServerError(accessOrchestrationServiceException);
+            }
         }
     }
 }
